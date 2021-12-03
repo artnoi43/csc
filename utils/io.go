b5ab4217011bef8174d3ff03db7ba608def90065
artnoi <artdrawin@gmail.com>
2021-12-04 02:34:30 +0700

    update io.go

--- utils/io.go
+++ utils/io.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
 
@@ -12,12 +13,16 @@ func ReadFile(path string) ([]byte, error) {
 	var buf = new(bytes.Buffer)
 	fp, err := os.Open(path)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to open file")
+		var emptyPath string
+		if len(path) == 0 {
+			emptyPath = "\nReminder: specify path with -f\n"
+		}
+		return nil, errors.Wrap(err, fmt.Sprintf("%sfailed to open file '%s'", emptyPath, path))
 	}
 	defer fp.Close()
 	_, err = io.Copy(buf, fp)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to read file")
+		return nil, errors.Wrap(err, fmt.Sprintf("failed to read file '%s'", path))
 	}
 	return buf.Bytes(), nil
 }
