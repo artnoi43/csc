4738e2b952153481402370929246ef989228eafc
artnoi <artdrawin@gmail.com>
2021-12-04 23:36:56 +0700

    refactor

--- lib/algo.go
+++
@@ -1,9 +0,0 @@
-package libcsc
-
-type Algo string
-
-const (
-	SHA256 = "SHA256"
-	SHA1   = "SHA1"
-	MD5    = "MD5"
-)
--- lib/algorithms.go
+++ lib/algorithms.go
@@ -1,6 +1,13 @@
 package libcsc
 
+type Algo string
 type cscFunc func([]byte, string) (string, bool)
+
+const (
+	SHA256 = "SHA256"
+	SHA1   = "SHA1"
+	MD5    = "MD5"
+)
 
 var FuncMap = map[Algo]cscFunc{
 	SHA256: SHA256Sum,
